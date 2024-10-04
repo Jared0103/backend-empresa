8dedbb0ee19231822d27e9009ba87fbca52d11b7
Jared0103 <josejared8ac@hotmail.com>
2024-10-04 08:40:59 -0600

    listo

---
+++ src/routes/empleadoRoutes.js
@@ -0,0 +1,69 @@
+import express from 'express'
+import authMiddleware from '../middleware/authMiddleware.js'
+import { check } from 'express-validator'
+import multer from 'multer'
+import {
+    createEmpleado,
+    updateEmpleado,
+    deleteEmpleado,
+    getAllEmpleados,
+    getEmpleadoById,
+    getEmpleadoByUsername,
+    getEmpleadoByRol
+} from '../controllers/empleadoController.js'
+
+const upload = multer({ storage: multer.memoryStorage() })
+const router = express.Router()
+
+router.post(
+    '/create',
+    authMiddleware, // Aquí agregas el middleware de autenticación
+    upload.single('imagen'),
+    [
+        check('nombre').notEmpty().withMessage('El nombre es obligatorio'),
+        check('usuario').notEmpty().withMessage('El usuario es obligatorio'),
+        check('password')
+            .isLength({ min: 6 })
+            .withMessage('La contraseña debe tener mínimo 6 caracteres')
+    ],
+    createEmpleado
+)
+
+router.put(
+    '/update/:id',
+    authMiddleware,
+    upload.single('imagen'),
+    updateEmpleado
+)
+
+router.delete(
+    '/delete/:id',
+    authMiddleware,
+    deleteEmpleado
+)
+
+router.get(
+    '/',
+    authMiddleware,
+    getAllEmpleados
+)
+
+router.get(
+    '/empleado/:id',
+    authMiddleware,
+    getEmpleadoById
+)
+
+router.get(
+    '/rol/:rol',
+    authMiddleware,
+    getEmpleadoByRol
+)
+
+router.get(
+    '/usuario/:usuario',
+    authMiddleware,
+    getEmpleadoByUsername
+)
+
+export default router
