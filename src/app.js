6aa38a1ecdbc70c9b114300996f4807590280a30
Jared0103 <josejared8ac@hotmail.com>
2024-10-08 09:29:06 -0600

    configure cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ import dotenv from 'dotenv'
 import errorHandler from './middleware/errorHandler.js'
 import rateLimitMiddleware from './middleware/rateLimit.js'
 import routes from './routes/index.js'
+import cors from 'cors'
 
 dotenv.config()
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
 const app = express()
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(rateLimitMiddleware)
 app.use('api/v1', routes)
